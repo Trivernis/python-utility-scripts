545f6f96d1008cb39b0d19306fb107275e6d3b5c
Trivernis <trivernis@hotmail.de>
2018-10-11 20:27:34 +0200

    Renamed local Repo
    - new name is python-utility-scripts

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='utilities',
+    name='python-utility-scripts',
     version='0.1',
     description='Some utility-scripts for python.',
     url='https://github.com/trivernis/python-utility-scripts',
     author='trivernis',
     license='GPLv3',
-    packages=['utilities'],
+    packages=['python-utility-scripts'],
     install_requires=[
         'bs4',
         'lxml'
